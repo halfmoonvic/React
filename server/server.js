1d2640884de001fa9a67729ce9575ca0e339ed46
cs-300 <halfmoonvic@gmail.com>
2017-11-06 18:32:14 +0800

    chapter2(mongodb): Learn mongodb 学习了下mongodb的增删改查的方法

--- server/server.js
+++ server/server.js
@@ -1,11 +1,37 @@
 const express = require('express')
 const mongoose = require('mongoose')
-// 链接
-const DB_URL = 'mongodb://localhost:27017'
+// 链接mongo 并且使用 imooc 这个集合
+const DB_URL = 'mongodb://localhost:27017/imooc'
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected', function () {
   console.log('mongo connect success')
 })
+// 类似于 mysql的表 mongo里有文档、字段的概念
+const User = mongoose.model('user', new mongoose.Schema({
+  user: {type: String, require: true},
+  age: {type: Number, require: true}
+}))
+
+// 新增数据
+// User.create({
+//   user: 'cs',
+//   age: 27
+// }, function (err, doc) {
+//   if (!err) {
+//     console.log(doc)
+//   } else {
+//     console.log(err)
+//   }
+// })
+
+// 删除数据
+// User.remove({_id: '5a0038d4d6dcdd35f4cc0502'}, function (err, doc) {
+//   console.log(doc)
+// })
+// 更新数据
+// User.update({'user': 'xiaohua'}, {'$set': {age: 26}}, function (err, doc) {
+//   console.log(doc)
+// })
 // 新建app
 const app = express()
 
@@ -14,10 +40,19 @@ app.get('/', function (req, res) {
 })
 
 app.get('/data', function (req, res) {
-  // res.json({name: "cs", height: 187})
-  res.status(200).json({'key': '188', 'hehe': '厉害了，我的哥'})
-  // res.status(200).json(obj)
+  // 查找到一条数据即返回（对象）
+  // User.findOne({}, function (err, doc) {
+    // res.json(doc)
+  // })
+  // 查找所有的数据
+  User.find({}, function (err, doc) {
+    res.json(doc)
+  })
+  // res.status(200).json({'key': '188', 'hehe': '厉害了，我的哥'})
 })
+// app.get('/delete', function () {
+
+// })
 
 app.listen(9093, function () {
   console.log('Node app start at port 9093')
