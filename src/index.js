42f5b24a57022bc163cb8e5d6834a74538f772a4
cs-575 <halfmoonvic@gmail.com>
2017-11-08 07:27:57 +0800

    chapter4(redux): 4.1节 学习redux基本的方法

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// 1. 新建store
+// 通过 reducer 遍历
+// 根据老的state和action 生成新的state
+function counter(state=0, action) {
+  switch(action.type) {
+    case '加机关枪':
+      return state + 1
+    case '减机关枪':
+      return state - 1
+    default:
+      return 10
+  }
+}
+
+const store = createStore(counter)
+
+const init = store.getState()
+console.log(init)
+
+function listener() {
+  const current = store.getState()
+  console.log(`现在有机关枪${current}把`)
+}
+store.subscribe(listener)
+
+// 派发事件
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '减机关枪'})
+store.dispatch({type: '减机关枪'})
+store.dispatch({type: '减机关枪'})
