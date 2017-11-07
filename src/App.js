cb65ae071a12d0aea78d9dc94281fed2f2201be3
cs-575 <halfmoonvic@gmail.com>
2017-11-07 08:18:55 +0800

    chapter3(base): React基础学习

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+class App extends Component{
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    const boss = '李云龙'
+    return(
+      <div>
+        <h2>独立团, 团长{boss}</h2>
+        <Yiying />
+        <Qibing boss="孙德胜" />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+function Qibing(props) {
+  return <h2>{props.boss}</h2>
+}
+class Yiying extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const boss = '张达标'
+    return <h2>{boss}</h2>
+  }
+}
+
+export default App
