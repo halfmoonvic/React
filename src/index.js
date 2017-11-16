992354b3251c4070af634c64342ec6f9cf921b9d
cs-300 <halfmoonvic@gmail.com>
2017-11-16 18:52:44 +0800

    chapter4(react-router): 基础学习

--- src/index.js
+++ src/index.js
@@ -1,29 +1,47 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import App from './app'
+
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+
 import { counter } from './index.redux'
+import App from './app'
 
 const store = createStore(counter, compose(
   applyMiddleware(thunk),
   window.devToolsExtension ? window.devToolsExtension() : ()=>{}
 ))
 
-// function render() {
-//   ReactDom.render(
-//     <App store={store} actionAdd={addGun} actionJian={removeGun} actionSync={addGunAsync} />,
-//     document.getElementById('root')
-//   )
-// }
-// render()
-
-// store.subscribe(render)
+function Erying() {
+  return <h2>二营组件</h2>
+}
+function Qibinglian() {
+  return <h2>骑兵连组件</h2>
+}
 
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>一营</Link>
+          </li>
+          <li>
+            <Link to='/erying'>二音</Link>
+          </li>
+          <li>
+            <Link to='/qibinglian'>骑兵连</Link>
+          </li>
+        </ul>
+        <Route path='/' exact component={App}></Route>
+        <Route path='/erying' component={Erying}></Route>
+        <Route path='/qibinglian' component={Qibinglian}></Route>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
