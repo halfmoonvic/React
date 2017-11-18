1a28ffebb44a41f83be2763fd6ecf17159192b16
cs-300 <halfmoonvic@gmail.com>
2017-11-18 16:35:43 +0800

    redux(sum-up): 传值payload的方式 <button onClick={() => store.dispatch(this.props.propAdd(2))}>加</button> 如果直接写成 <button onClick={store.dispatch(this.props.propAdd(2))}>加</button> 则 onCick 的执行 成为一个对象（action）。需要用箭头函数 承载一下

--- src/app.js
+++ src/app.js
@@ -7,23 +7,27 @@ import { addCounter, removeCounter, chengCounter, chuCounter } from './index.red
   // 你要state什么属性放到props里
   state=>({states: state}),
   // 你要什么方法，放到props里，自动dispatch
-  {addCounter: addCounter, removeCounter, chengCounter, chuCounter}
+  {addCounter, removeCounter, chengCounter, chuCounter}
 )
 
 class App extends React.Component {
+  componentDidMount() {
+  }
   render() {
     // const store = this.props.store
-    console.log('渲染ing')
-    console.log(this.props)
-    console.log(this.props.addCounter)
+    console.log('render')
+
+    // console.log(this.props)
+    console.log(() => this.props.addCounter(2))
+    // console.log(this.props.addCounter(2))
     return (
       <div>
         <h1>加减state：{this.props.states.addRemoveCounter}</h1>
         <h1>乘除state：{this.props.states.chengChuCounter}</h1>
-        <button onClick={this.props.addCounter}>加</button>
-        <button onClick={this.props.removeCounter}>减</button>
-        <button onClick={this.props.chengCounter}>乘</button>
-        <button onClick={this.props.chuCounter}>除</button>
+        <button onClick={() => this.props.addCounter(2)}>加</button>
+        <button onClick={() => this.props.removeCounter(2)}>减</button>
+        <button onClick={() => this.props.chengCounter(2)}>乘</button>
+        <button onClick={() => this.props.chuCounter(2)}>除</button>
       </div>
     )
   }
