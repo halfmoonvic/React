7407f76debc131c98733f5718b152d43fc792a10
cs-300 <halfmoonvic@gmail.com>
2017-11-22 15:44:22 +0800

    chapter6(end): 登录注册－cookie保存登录状态

--- server/user.js
+++ server/user.js
@@ -5,9 +5,23 @@ const Router = express.Router()
 const model = require('./model')
 const User = model.getModel('user')
 
+const _filter = {'pwd': 0, '__v': 0}
+
 Router.get('/list', function (req, res) {
+  // User.remove({}, function (err, doc) {})
   User.find({}, function (err, doc) {
     return res.json(doc)
+  })
+})
+
+Router.post('/login', function (req, res) {
+  const {user, pwd} = req.body
+  User.findOne({user, pwd: md5Pwd(pwd)}, _filter, function (err, doc) {
+    if (!doc) {
+      return res.json({code: 1, msg: '用户名不存在或者密码错误'})
+    }
+    res.cookie('userid', doc._id)
+    return res.json({code: 0, msg: doc})
   })
 })
 
@@ -17,18 +31,39 @@ Router.post('/register', function (req, res) {
     if (doc) {
       return res.json({code: 1, msg: '用户名重复'})
     }
-    User.create({user, pwd: md5Pwd(pwd), type}, function (err, doc) {
-      if (err) {
+
+    const userModel = new User({user, type, pwd: md5Pwd(pwd)})
+    userModel.save(function (e, d) {
+      if (e) {
         return res.json({code: 1, msg: '后台出错了'})
       }
-      return res.json({code: 0})
+      const {user, type, _id} = d
+      res.cookie('userid', _id)
+      return res.json({code: 0, data: {user, type, _id}})
     })
+    // User.create({user, pwd: md5Pwd(pwd), type}, function (err, doc) {
+    //   if (err) {
+    //     return res.json({code: 1, msg: '后台出错了'})
+    //   }
+    //   return res.json({code: 0})
+    // })
   })
 })
 
 Router.get('/info', function (req, res) {
   // 用户有没有 cookie
-  return res.json({code: 1})
+  const { userid } = req.cookies
+  if (!userid) {
+    return res.json({code: 1})
+  }
+  User.findOne({_id: userid}, _filter, function (err, doc) {
+    if (err) {
+      return res.json({code: 1, msg: '后端出错'})
+    }
+    if (doc) {
+      return res.json({code: 0, data: doc})
+    }
+  })
 })
 
 // 加密加盐
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -3,13 +3,14 @@ import { getRedirectPath } from './../util'
 
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 const ERROR_MSG = 'ERROR_MSG'
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+const LOAD_DATA = 'LOAD_DATA'
 
 const initState = {
   redirectTo: '',
   isAuth: '',
   msg: '',
   user: '',
-  pwd: '',
   type: ''
 }
 
@@ -25,6 +26,19 @@ export function user(state = initState, action) {
         ...payload,
         redirectTo: getRedirectPath(action.payload)
       }
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        msg: '',
+        isAuth: true,
+        ...payload,
+        redirectTo: getRedirectPath(action.payload)
+      }
+    case LOAD_DATA:
+      return {
+        ...state,
+        ...action.payload
+      }
     case ERROR_MSG:
       return {
         ...state,
@@ -36,12 +50,21 @@ export function user(state = initState, action) {
   }
 }
 
+
 function registerSuccess(data) {
   return { type: REGISTER_SUCCESS, payload: data }
 }
 
 function errorMsg(msg) {
   return { msg, type: ERROR_MSG }
+}
+
+export function loadData(userinfo) {
+  return {type: LOAD_DATA, payload: userinfo}
+}
+
+function loginSuccess(data) {
+  return { type: LOGIN_SUCCESS, payload: data }
 }
 
 export function register({ user, pwd, repeatpwd, type }) {
@@ -56,6 +79,23 @@ export function register({ user, pwd, repeatpwd, type }) {
       .then((res) => {
         if (res.status === 200 && res.data.code === 0) {
           dispatch(registerSuccess({ user, pwd, type }))
+        } else {
+          dispatch(errorMsg(res.data.msg))
+        }
+      })
+  }
+}
+
+export function login({user, pwd}) {
+  if (!user || !pwd) {
+    return errorMsg('用户名密码必须输入')
+  }
+  return dispatch => {
+    axios.post('/user/login', {user, pwd})
+      .then(res => {
+        if (res.status === 200 && res.data.code === 0) {
+          console.log(res.data.msg)
+          dispatch(loginSuccess(res.data.msg))
         } else {
           dispatch(errorMsg(res.data.msg))
         }
