d4f2ff7d8bff203ae7d0b98d386a711aef54a898
cs-300 <halfmoonvic@gmail.com>
2017-12-11 10:16:21 +0800

    shouldComponentUpdate: 与性能优化相关了

--- src/app.js
+++ src/app.js
@@ -1,44 +1,39 @@
 import React from 'react'
 // 通过 connect 来调用
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { setAdd, setRemove, setCheng, setChu } from './store/actions'
 import { getAdd, getRemove, getCheng, getChu } from './store/async-actions'
 
-// @connect(
-//   // 你要state什么属性放到props里
-//   state => ({ states: state }),
-//   // 你要什么方法，放到props里，自动dispatch
-//   { setAdd, setRemove, setCheng, setChu, getAdd, getRemove, getCheng, getChu }
-// )
-
 class App extends React.Component {
-  componentDidMount() {}
+  constructor(props) {
+    super(props)
+    this.state = {
+      selfNum: 0
+    }
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps, nextState)
+    if (nextProps.states.addRemoveCounter % 5 === 0 || nextProps.states.chengChuCounter % 3 === 0) {
+      return true
+    }
+    return false
+  }
   render() {
-    // const store = this.props.store
     console.log('render')
-      // console.log(this.props)
     return (
       <div>
         <h1>加减state：{this.props.states.addRemoveCounter}</h1>
         <h1>乘除state：{this.props.states.chengChuCounter}</h1>
+        <h1>selfNum：{this.state.selfNum}</h1>
         <button onClick={() => this.props.setAdd(2)}>加</button>
         <button onClick={() => this.props.setRemove(2)}>减</button>
-        <button onClick={() => this.props.getCheng(3)}>乘</button>
-        <button onClick={() => this.props.getChu(2)}>除</button>
+        <button onClick={() => this.props.setCheng(3)}>乘</button>
+        <button onClick={() => this.props.setChu(2)}>除</button>
+        <button onClick={() => { this.setState({selfNum: this.state.selfNum + 1}) }}>control self num</button>
       </div>
     )
   }
 }
-
-// connect 接受四个参数，一般只是定义前两个就好了
-// connect([mapStateToProps], [mapDispatchToProps], [mergeProps], [options])
-
-// 方式一 直接写在上面
-// App = connect(
-//   state => ({ states: state }),
-//   { setAdd, setRemove, setCheng, setChu, getAdd, getRemove, getCheng, getChu }
-// )(App)
 
 // 方式二 单提出来
 const mapStateToProps = (state) => ({ states: state })
