77a551d0eca90821ab1fe9f7e801a21523598fa2
cs-300 <halfmoonvic@gmail.com>
2017-12-14 17:07:51 +0800

    componentDidCatch(错误处理): 错误处理机制

--- src/app.js
+++ src/app.js
@@ -7,10 +7,25 @@ import { connect } from './mini-redux/mini-react-redux.js'
 import { setAdd, setRemove, getAdd, getRemove, arrAdd } from './mini-redux/test-miniredux.js'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      releaseBugs: false
+    }
+    this.generateError = this.generateError.bind(this)
+  }
+  generateError() {
+    this.setState({
+      releaseBugs: true
+    })
+  }
   render() {
-    console.log(this.props)
+    if (this.state.releaseBugs) {
+      throw new Error("I crashed!")
+    }
     return (
       <div>
+        <button onClick={this.generateError}>生成错误</button>
         <h2>现在的数值是 {this.props.num}</h2>
         <button onClick={() => this.props.setAdd(2)}>加</button>
         <button onClick={() => this.props.setRemove(3)}>减</button>
--- src/index.js
+++ src/index.js
@@ -12,9 +12,35 @@ import Performance from './test-performance.js'
 
 const store = createStore(counter, applyMiddleware(thunk, arrThunk))
 
+class ErrComp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      errorInfo: null
+    }
+  }
+  componentDidCatch(error, errorInfo) {
+    console.log(error)
+    console.log(errorInfo.componentStack)
+    this.setState({
+      error: true,
+      errorInfo: errorInfo
+    })
+  }
+  render() {
+    if (this.state.error) {
+      return <div>有错误</div>
+    }
+    return this.props.children
+  }
+}
+
 ReactDom.render(
   <Provider store={store}>
-    <Performance></Performance>
+    <ErrComp>
+      <App></App>
+    </ErrComp>
   </Provider>,
   document.getElementById('root')
 )
